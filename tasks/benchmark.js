cd2ae70151d15202e16adc449f2322d4d150646e
Fabio Spampinato <spampinabio@gmail.com>
2023-02-15 18:33:50 +0000

    Avoiding committing the benchmark fixture

--- tasks/benchmark.js
+++ tasks/benchmark.js
@@ -6,7 +6,9 @@ import fs from 'node:fs';
 import U8 from 'uint8-encoding';
 import Base128 from '../dist/index.js';
 
-const WAP = fs.readFileSync ( './tasks/war_and_peace.txt', 'utf8' );
+/* HELPERS */
+
+const WAP = fs.readFileSync ( './tasks/fixture.txt', 'utf8' );
 const WAP_UINT8 = U8.encode ( WAP );
 const WAP_ENCODED = Base128.encodeStr ( WAP );
 const WAP_ENCODED_UINT8 = Base128.encode ( WAP_UINT8 );
